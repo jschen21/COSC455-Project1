d4ef3dc258662648b54c2015b78a5b1ddc1f2faf
jchen21 <jonathan.s.chen3@gmail.com>
2017-10-25 19:23:05 -0400

    Added BNF file

--- src/main/scala/edu/towson/cosc/cosc455/jchen21/project1/CONSTANTS.scala
+++ src/main/scala/edu/towson/cosc/cosc455/jchen21/project1/CONSTANTS.scala
@@ -5,6 +5,7 @@ object CONSTANTS {
   val DOCE : String = 	"\\END"
   val TITLEB : String = "\\TITLE["
   val BRACKETE : String = "]"
+
   val letters : List[String] = List("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
   val numbersEtc : List[String] = List("1","2","3","4","5","6","7","8","9","0",",",".","\"",":","?","_","/", "'", "")
   val whiteSpace : List[String] = List(" ", "\t", "\n", "\b","\f","\r")
